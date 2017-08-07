84fadf9dfe66c256a816800bb87e9a33b8e14a06
Pascal Rhod <pascal.rhod@canaltp.fr>
2017-08-07 16:36:22 +0200

    fixing install test on avignon

--- tests/test_avignon.sh
+++ tests/test_avignon.sh
@@ -1,9 +1,10 @@
 wget http://download.geofabrik.de/europe/france/provence-alpes-cote-d-azur-latest.osm.pbf -O /data/provence-alpes-cote-d-azur-latest.osm.pbf
 
-bash /data/backend/osmconvert/osmconvert /data/provence-alpes-cote-d-azur-latest.osm.pbf -B=avignon.poly -o=avignon.pbf
+cd /data/tests
+/data/backend/osmconvert/osmconvert /data/provence-alpes-cote-d-azur-latest.osm.pbf -B=avignon.poly -o=avignon.pbf
 
 rm /data/work/extracts/france_provence_alpes_cote_d_azur.osm.pbf
 mv avignon.pbf /data/work/extracts/france_provence_alpes_cote_d_azur.osm.pbf
 cd /data/backend
 source ./osmose-backend-venv/bin/activate
-python osmose_run.py --analyser=analyser_sax --country=france_provence_alpes_cote_d_azur --skip-download --no-clean
+python osmose_run.py --analyser=sax --country=france_provence_alpes_cote_d_azur --skip-download --no-clean
